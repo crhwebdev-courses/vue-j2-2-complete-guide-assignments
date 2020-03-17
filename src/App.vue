cd12339651e9a0703020990b37fe73b816c0fa45
Carl Hain <crhain@gmail.com>
2020-03-17 17:53:09 -0500

    Pass some html content to each color component using slot

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
-                <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button class="btn btn-primary">Load Blue Template</button>
+        <button class="btn btn-success">Load Green Template</button>
+        <button class="btn btn-danger">Load Red Template</button>
+        <hr />
+        <app-blue>
+          <h1>This the Blue Component</h1>
+        </app-blue>
+        <app-green>
+          <h1>This the Green Component</h1>
+        </app-green>
+        <app-red>
+          <h1>This the Red Component</h1>
+        </app-red>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
-    }
+export default {
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>

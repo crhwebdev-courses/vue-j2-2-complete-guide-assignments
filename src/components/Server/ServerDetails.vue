588dcb4ce27d1c4d1493a2656d11b75868a7ded7
Carl Hain <crhain@gmail.com>
2020-03-16 20:31:47 -0500

    ServerDetials shows default message if details property is null or shows message that server detials are updated

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
-    </div>
-
+  <div class="col-xs-12 col-sm-6">
+    <p v-if="!details">Server Details are currently not updated</p>
+    <p v-else>Server details are updated</p>
+  </div>
 </template>
 
 <script>
+export default {
+  name: "ServerDetails",
+  data: function() {
+    return {
+      details: null
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>

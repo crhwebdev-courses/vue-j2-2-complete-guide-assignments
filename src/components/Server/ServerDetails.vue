543d8d8fde9dc7c926302f775a99de5df0bffce3
Carl Hain <crhain@gmail.com>
2020-03-16 21:30:40 -0500

    Display server status for selected server in ServerDetail component

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-xs-12 col-sm-6">
     <p v-if="!details">Server Details are currently not updated</p>
-    <p v-else>Server details are updated</p>
+    <p v-else>Server {{ details.id }}: {{ details.status }}</p>
   </div>
 </template>
 

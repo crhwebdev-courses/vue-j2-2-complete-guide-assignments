818c40e9f9d78d67fa4ab1ccb5c4822431554fa3
Carl Hain <crhain@gmail.com>
2020-03-19 13:27:10 -0500

    Add check to hide form after it has been submitted

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <form>
-      <div class="row">
+      <div class="row" v-if="!formSubmited">
         <div
           class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
         >

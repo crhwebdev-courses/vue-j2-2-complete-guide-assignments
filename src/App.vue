eddc3409ed2d6d34cbe45c31768132cb31d23ac5
Carl Hain <crhain@gmail.com>
2020-03-19 20:31:23 -0500

    Updated to take an object as argument to directive that contains several functions that will all get executed -- this was done becaus directives seem to no longer pass functions handed to them as arguments

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <h1>Directives Exercise</h1>
         <!-- Exercise -->
         <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-        <p v-myon:click="'alert(\'Hello World\');'">
+        <p v-myon:click="{ stuffios: stuff, otherStuff }">
           Click Me!
         </p>
       </div>
@@ -14,14 +14,31 @@
 </template>
 
 <script>
+// Note: for some reason, custom directives will not take a function any more...
+// therefore, we pass an object with a list of functions that all get exectuted
 export default {
   directives: {
     myon: {
       bind: function(el, binding, vnode) {
-        vnode.elm.addEventListener(binding.arg, e => {
-          eval(binding.value);
+        const fns = binding.value;
+        el.addEventListener(binding.arg, e => {
+          console.log("clicked");
+          for (let prop in fns) {
+            let fn = fns[prop];
+            if (fns.hasOwnProperty(prop) && typeof fn === "function") {
+              fn(e);
+            }
+          }
         });
       }
+    }
+  },
+  methods: {
+    stuff(e) {
+      console.log("stuff");
+    },
+    otherStuff(e) {
+      console.log("other stuff is running now");
     }
   }
 };

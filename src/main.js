014c1772787cbd45b8ae70546b63fc7f2f21d8a6
Carl Hain <crhain@gmail.com>
2020-03-20 18:06:43 -0500

    complete exercise 1 -- build local filter to reverse some text

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.filter('to-lowercase', function(value) {
-    return value.toLowerCase();
+Vue.filter("to-lowercase", function(value) {
+  return value.toLowerCase();
+});
+
+/*Exercise 2 -->
+        <!-- Build a global Filter which counts the length of a word and it appends it -->
+        <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+*/
+Vue.filter("append-length", function(value) {
+  let len = value.length;
+  return `${value} (${len})`;
 });
 
 Vue.mixin({
-    created() {
-        console.log('Global Mixin - Created Hook');
-    }
+  created() {
+    console.log("Global Mixin - Created Hook");
+  }
 });
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});

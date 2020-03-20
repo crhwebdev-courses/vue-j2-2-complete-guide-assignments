b9161f4d4a1cbc0144b9a1265bafcd60f855446f
Carl Hain <crhain@gmail.com>
2020-03-19 19:32:21 -0500

    Adding a custom 'myon' directive now will evaluate the passed value for the given event

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+        <p v-myon:click="'alert(\'Hello World\');'">
+          Click Me!
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {}
+export default {
+  directives: {
+    myon: {
+      bind: function(el, binding, vnode) {
+        vnode.elm.addEventListener(binding.arg, e => {
+          eval(binding.value);
+        });
+      }
+    }
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
